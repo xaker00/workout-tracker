b871b8073095fe0074c0c632a547e62e8f6f37f6
Ilya Spivakov <xaker00@gmail.com>
2021-10-21 22:14:25 -0700

    added model and updated seed

---
+++ models/index.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    required: true,
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = { Workout };
+
+/*
+ day: new Date(new Date().setDate(new Date().getDate() - 1)),
+ exercises: [
+   {
+     type: 'resistance',
+     name: 'Military Press',
+     duration: 20,
+     weight: 300,
+     reps: 10,
+     sets: 4,
+   },
+ ],
+ */
